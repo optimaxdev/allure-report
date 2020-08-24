db01ff4a5110b11e5b570bff4e1c5ff5c2c31ff6
Ilya Kunafin <bankinobi@gmail.com>
2020-08-24 12:42:33 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+> GIthub action to generate allure report and post comment with report link on PR
+
+## Inputs
+- `token`: Github token
+- `body`: Comment body
+- `pr_number`: Number of PR to comment
+- `project_id`: Allure-server project id to associate report with (e.g. gusa-desktop-integrations)
+- `results_directory`: Directory with allure-results xml files (e.g. __specs__/allure-results)
+  
---
+++ main.py
@@ -0,0 +1,146 @@
+import re
+import sys
+import os
+import requests
+import json
+import base64
+
+GITHUB_API_BASE_URL = 'https://api.github.com'
+ALLURE_SERVER_URL = 'http://allure.gusadev.com'
+
+
+def escape(v: str) -> str:
+    return repr(v)[1:-1]
+
+
+def print_action_error(msg: str):
+    sys.stdout.write(f'::error file={__name__}::{escape(msg)}\n')
+
+
+def get_action_input(name):
+    var = os.environ.get(f'INPUT_{name.upper()}', '')
+    if var == '':
+        print_action_error(f'Input required and not supplied: {name}')
+        exit(1)
+    return var
+
+
+def report_files(results_directory):
+    results_directory = os.path.dirname(
+        os.path.realpath(__file__)) + results_directory
+    files = os.listdir(results_directory)
+    results = []
+    for file in files:
+        result = {}
+
+        file_path = results_directory + "/" + file
+
+        if os.path.isfile(file_path):
+            try:
+                with open(file_path, "rb") as f:
+                    content = f.read()
+                    if content.strip():
+                        b64_content = base64.b64encode(content)
+                        result['file_name'] = file
+                        result['content_base64'] = b64_content.decode('UTF-8')
+                        results.append(result)
+                    else:
+                        print('Empty File skipped: ' + file_path)
+            finally:
+                f.close()
+        else:
+            print('Directory skipped: ' + file_path)
+    request_body = {
+        "results": results
+    }
+    json_request_body = json.dumps(request_body)
+    return json_request_body
+
+
+def generate_report(project_id, execution_name, execution_from, report_body):
+    execution_type = 'github-actions'
+    headers = {'Content-type': 'application/json'}
+
+    print("Uploading test results")
+    send_response = requests.post(
+        f'{ALLURE_SERVER_URL}/allure-docker-service/send-results?project_id={project_id}',
+        headers=headers,
+        data=report_body,
+    )
+    print(f'Response code: {send_response.status_code}')
+    print(json.loads(send_response.content['meta_data']['message']))
+
+    print("Generating Allure report")
+    generate_response = requests.get(
+        f'{ALLURE_SERVER_URL}/allure-docker-service/generate-report?project_id={project_id}&execution_name={execution_name}&execution_from={execution_from}&execution_type={execution_type}',
+        headers=headers,
+        data=report_body,
+    )
+    report_url = json.loads(generate_response.content['data']['report_url'])
+    print(f'Response code: {generate_response.status_code}')
+    return report_url
+
+
+def find_allure_comments(token, repo, pr_number, regexp):
+    headers = {'Authorization': 'Bearer ' + token}
+
+    response = requests.get(
+        f'{GITHUB_API_BASE_URL}/repos/{repo}/issues/{pr_number}/comments',
+        headers=headers,
+    )
+    print(f'Response code: {response.status_code}')
+    comment_ids = [comment['id'] for comment in json.loads(
+        response.content) if re.match(regexp, comment['body'])]
+    return comment_ids
+
+
+def post_allure_comment(token, repo, pr_number, body, comment_ids, report_url):
+    headers = {'Authorization': f'token {token}'}
+    data = {"body": f"{body}: {report_url}"}
+
+    while len(comment_ids) > 1:
+        print('Purging unnecessary Allure report links')
+        for comment in comment_ids:
+            print(f'Purging commend with ID: {comment}')
+            response = requests.delete(
+                f'{GITHUB_API_BASE_URL}/repos/{repo}/issues/comments/{comment}',
+                headers=headers,
+                data=json.dumps(data))
+            print(f'Response code: {response.status_code}')
+            comment_ids.remove(comment)
+        break
+
+    if len(comment_ids) == 0:
+        print('Posting PR comment with Allure report link')
+        response = requests.post(f'{GITHUB_API_BASE_URL}/repos/{repo}/issues/{pr_number}/comments',
+                                 headers=headers,
+                                 data=json.dumps(data))
+        print(f'Response code: {response.status_code}')
+
+    elif len(comment_ids) == 1:
+        print('Editing PR comment with latest Allure report link')
+        response = requests.patch(f'{GITHUB_API_BASE_URL}/repos/{repo}/issues/comments/{comment_ids[0]}',
+                                  headers=headers,
+                                  data=json.dumps(data))
+        print(f'Response code: {response.status_code}')
+
+
+def main():
+    token = get_action_input('token')
+    repo = os.environ['GITHUB_REPOSITORY']
+    body = get_action_input('body')
+    pr_number = get_action_input('pr_number')
+    project_id = get_action_input('project_id')
+    results_directory = get_action_input('results_directory')
+    execution_name = pr_number
+    execution_from = f'https://github.com/{repo}/pull/{pr_number}'
+
+    report_body = report_files(results_directory)
+    report_url = generate_report(
+        project_id, execution_name, execution_from, report_body)
+    comment_ids = find_allure_comments(token, repo, pr_number, body)
+    post_allure_comment(token, repo, pr_number, body, comment_ids, report_url)
+
+
+if __name__ == '__main__':
+    main()
